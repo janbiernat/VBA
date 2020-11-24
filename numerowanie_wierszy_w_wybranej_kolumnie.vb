5201e7f01944df5f6503f9fbcfdba8dc9a6fc667
Jan T. Biernat <46463762+janbiernat@users.noreply.github.com>
2020-11-24 10:31:39 +0100

    Add files via upload

---
+++ numerowanie_wierszy_w_wybranej_kolumnie.vb
@@ -0,0 +1,22 @@
+﻿Sub WierszeNrP(ArkuszNazwa As String, WierszNrOd As Long, KolumnaNr As Long)
+'WierszeNrP - Numerowanie wierszy w wybranej kolumnie.
+'Copyright (c)by Jan T. Biernat
+'
+'Wywołanie procedury:
+'Call WierszeNumerowanie(zmienna_tekstowa, zmienna_liczbowa)
+'
+    Dim Licznik As Long
+    Licznik = 0
+    With Sheets(ArkuszNazwa)
+        If ((KolumnaNr > 1) And (WierszNrOd > 0)) Then
+            Do
+                Licznik = Licznik + 1
+                If (.Cells(WierszNrOd + Licznik, KolumnaNr).Value <> "") Then
+                    If (.Cells(WierszNrOd + Licznik, KolumnaNr - 1).Value = "") Then
+                        .Cells(WierszNrOd + Licznik, KolumnaNr - 1).Value = Licznik
+                    End If
+                End If
+            Loop Until (.Cells(WierszNrOd + Licznik, KolumnaNr).Value = "")
+        End If
+    End With
+End Sub
