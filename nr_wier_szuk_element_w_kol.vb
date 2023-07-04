c944be37287f2d1baed1477435f6e7b3421f08c0
Jan T. Biernat <46463762+janbiernat@users.noreply.github.com>
2023-07-04 23:50:22 +0200

    Add files via upload

---
+++ nr_wier_szuk_element_w_kol.vb
@@ -0,0 +1,32 @@
+Function NrWierSzukElementK(ArkuszNazwa As String, Szukaj As String, NrKol As Integer, KolM As Integer) As Integer
+'NrWierSzukElementK - Funkcja zwraca nr wiersza, w którym znajduje się szukany element.
+'Copyright (c)by Jan T. Biernat
+'=
+'Wywołanie funkcji: Cells(3, 4).Value = NrWierSzukElementK("Informacje o produktach", "telewizory LCD", 2, 999)
+'
+    Dim NrWier As Integer
+    Dim I As Integer
+    NrWier = 0
+    I = 0
+    ArkuszNazwa = Trim(ArkuszNazwa)
+    Szukaj = Trim(Szukaj)
+    If ((ArkuszNazwa <> "") And (Szukaj <> "")) Then
+        If (NrKol < 1) Then
+            NrKol = 1
+        End If
+        If (KolM < 1) Then
+            KolM = 1
+        End If
+        With Sheets(ArkuszNazwa)
+            For I = 1 To KolM
+                If (LCase(Trim(.Cells(I, NrKol).Value)) = LCase(Szukaj)) Then
+                    NrWier = I
+                    Exit For
+                End If
+            Next I
+        End With
+        NrWierSzukElementK = NrWier
+    Else
+        NrWierSzukElementK = "BŁĄD -?Parametry funkcji są błędne!"
+    End If
+End Function
