b5f608d3a0a4e29e1c30a43eed207584e8ffb9f4
Jan T. Biernat <46463762+janbiernat@users.noreply.github.com>
2023-07-07 23:59:36 +0200

    Add files via upload

---
+++ vba_tabliczka_mnozenia_nauka_worksheet_selectionchange.vb
@@ -0,0 +1,8 @@
+Private Sub Worksheet_SelectionChange(ByVal Target As Range)
+'Worksheet_SelectionChange - Procedura wykonuje instrukcje
+'                            lub wywołuje inne procedury/funkcje
+'                            w momencie zmiany zawartości
+'                            komórki.
+'
+    Call Sprawdz
+End Sub
