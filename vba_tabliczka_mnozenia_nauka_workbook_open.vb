9b34545fb09c311b4727737c903f22c794bd39fb
Jan T. Biernat <46463762+janbiernat@users.noreply.github.com>
2023-07-07 23:59:21 +0200

    Add files via upload

---
+++ vba_tabliczka_mnozenia_nauka_workbook_open.vb
@@ -0,0 +1,7 @@
+Private Sub Workbook_Open()
+'Workbook_Open - Automatycznie wykonuje instrukcje
+'                lub wywołuje poszczególne procedury/funkcje
+'                w momencie uruchomienia skoroszytu.
+'
+    Call StartTM
+End Sub
